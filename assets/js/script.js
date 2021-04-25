e3b25517d815ff1989838bb83789170f4a63d377
Brad Smith <smithfamily42@gmail.com>
2021-04-25 01:33:58 -0700

    added the array for storing tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 // set the date
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+//objet to store tasks in localStorage
+var tasks = {
+    "0700": [],
+    "0800": [],
+    "0900": [],
+    "1000": [],
+    "1100": [],
+    "1200": [],
+    "1300": [],
+    "1400": [],
+    "1500": [],
+    "1600": [],
+    "1700": [],
+    "1800": [],
+    "1900": [],
+};
+
