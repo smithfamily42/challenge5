d0426da69bf88aee6b941025b513d092ca30479b
Brad Smith <smithfamily42@gmail.com>
2021-04-25 14:13:20 -0700

    adding timer function and text area

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-//objet to store tasks in localStorage
+//object to store tasks in localStorage
 var tasks = {
     "0700": [],
     "0800": [],
@@ -18,4 +18,90 @@ var tasks = {
     "1800": [],
     "1900": [],
 };
+//saves tasks to localStorage
+var setTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+//load tasks from localStorage
+var getTasks = function() {
+    var loadTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (loadTasks) {
+        tasks=loadTasks
+        $.each(tasks, function(hour, task) {
+            var hourDiv = $("#", + hour);
+            createTask(task, hourDiv);
+        })
+    }
+    checkTasks();
+}
+//create the task element
+var createTask = function (taskText, hourDiv) {
+    var taskDiv = hourDiv.find(".task");
+    var taskPar = $("<p>")
+                .addClass("description")
+                .text(taskText)
+                taskDiv.html(taskPar);
+}
 
+var checkTasks = function() {
+    var currentHour = moment().hour();
+    $(".task-info").each(function() {
+        var elementHour = parseInt($(this).attr("id"));
+
+        if (elementHour < currentHour) {
+            $(this).removeClass(["present", "future"]).addClass("past");
+        } else if (elementHour === currentHour) {
+            $(this).removeClass(["past, future"]).addClass("present");
+        } else {
+            $(this).removeClass(["past", "present"]).addClass("future");
+        }
+        })
+
+    };
+
+    var replaceText = function(textAreaEl) {
+        var taskInfo = textAreaEl.closest(".task-info");
+        var textArea = taskInfo.find("textarea");
+
+        var time = taskInfo.attr("id");
+        var text = textAera.val().trim();
+
+        tasks[time] = [text];
+        setTasks();
+
+        //replace the textarea element with p element
+        createTask(text, taskInfo);
+    }
+
+    //event handlers
+    $(".task").click(function() {
+        $("textarea").each(function() {
+            replaceText($(this));
+        })
+
+        //convert to text area
+        var time = $(this).closest(".task-info").attr("id");
+        if (parseInt(time) >= moment().hour()) {
+            var text = $(this).text();
+            var textInput = $("<textarea>")
+                .addClass("form-control")
+                .val(text);
+
+            $(this).html(textInput);
+            textInput.trigger("focus");
+        }
+    })
+
+    //save button
+    $(".saveBtn").click(function() {
+        replaceText($(this));
+    })
+
+    //update time on page
+    timeHour = 3600000 - today.milliseconds();
+    setTimeout(function() {
+        setInterval(checkTasks, 3600000)
+            
+        }, timeHour);
+    
+    getTasks();
