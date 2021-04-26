f3258566bf59fcdfa1ccd710bb70771399a13336
Brad Smith <smithfamily42@gmail.com>
2021-04-25 21:55:39 -0700

    fixed save feature

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Work Day Scheduler
 GitHub repo: https://github.com/smithfamily42/challenge5
 live URL: https://smithfamily42.github.io/challenge5/
+
+moment.js for time management
+JQuery
+JavaScript
+Bootstrap
+Font Awesome icons https://fontawesome.com/
+
+![](2021-04-25-21-54-42.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,79 @@
+
 // set the date
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+var now = moment();
+
 //object to store tasks in localStorage
-var tasks = {
-    "0700": [],
-    "0800": [],
-    "0900": [],
-    "1000": [],
-    "1100": [],
-    "1200": [],
-    "1300": [],
-    "1400": [],
-    "1500": [],
-    "1600": [],
-    "1700": [],
-    "1800": [],
-    "1900": [],
-};
-//saves tasks to localStorage
-var setTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+var workTasks = [
+    { time: "0700", task: "" },
+    { time: "0800", task: "" },
+    { time: "0900", task: "" },
+    { time: "1000", task: "" },
+    { time: "1100", task: "" },
+    { time: "1200", task: "" },
+    { time: "1300", task: "" },
+    { time: "1400", task: "" },
+    { time: "1500", task: "" },
+    { time: "1600", task: "" },
+    { time: "1700", task: "" },
+    { time: "1800", task: "" },
+    { time: "1900", task: "" }
+];
+
+
 //load tasks from localStorage
-var getTasks = function() {
-    var loadTasks = JSON.parse(localStorage.getItem("tasks"));
-    if (loadTasks) {
-        tasks=loadTasks
-        $.each(tasks, function(hour, task) {
-            var hourDiv = $("#", + hour);
-            createTask(task, hourDiv);
-        })
-    }
-    checkTasks();
+var loadTasks = JSON.parse(localStorage.getItem("tasks"));
+if (loadTasks) {
+    workTasks = loadTasks;
 }
+
 //create the task element
-var createTask = function (taskText, hourDiv) {
-    var taskDiv = hourDiv.find(".task");
-    var taskPar = $("<p>")
-                .addClass("description")
-                .text(taskText)
-                taskDiv.html(taskPar);
+workTasks.map((block, index) => {
+    var hourLabel = block.time;
+    var blockColor = colorRow(hourLabel);
+    var row =
+        '<div class ="task-info row" id="' + index + '"><div class="col-1 hour pt-4">'
+        + hourLabel + '</div><div class="col task"><textarea class="form-control '
+        + blockColor + '">' + block.task + '</textarea></div><button class="col-1 saveBtn d-flex justify-content-center align-items-center"><i class="mt-2 far fa-save fa-lg"></i></button></div>'
+
+    $(".container").append(row);
+
+});
+//saves tasks to localStorage
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(workTasks));
+};
+
+/* Color rows based on current time */
+function colorRow(time) {
+    var planNow = moment(now, "H A");
+    var planEntry = moment(time, "H A");
+    console.log(planEntry);
+    if (planNow.isBefore(planEntry) === true) {
+        return "future";
+    } else if (planNow.isAfter(planEntry) === true) {
+        return "past";
+    } else {
+        return "present";
+    }
 }
 
-var checkTasks = function() {
-    var currentHour = moment().hour();
-    $(".task-info").each(function() {
-        var elementHour = parseInt($(this).attr("id"));
-
-        if (elementHour < currentHour) {
-            $(this).removeClass(["present", "future"]).addClass("past");
-        } else if (elementHour === currentHour) {
-            $(this).removeClass(["past, future"]).addClass("present");
-        } else {
-            $(this).removeClass(["past", "present"]).addClass("future");
-        }
-        })
-
-    };
-
-    var replaceText = function(textAreaEl) {
-        var taskInfo = textAreaEl.closest(".task-info");
-        var textArea = taskInfo.find("textarea");
-
-        var time = taskInfo.attr("id");
-        var text = textAera.val().trim();
-
-        tasks[time] = [text];
-        setTasks();
-
-        //replace the textarea element with p element
-        createTask(text, taskInfo);
-    }
-
-    //event handlers
-    $(".task").click(function() {
-        $("textarea").each(function() {
-            replaceText($(this));
-        })
-
-        //convert to text area
-        var time = $(this).closest(".task-info").attr("id");
-        if (parseInt(time) >= moment().hour()) {
-            var text = $(this).text();
-            var textInput = $("<textarea>")
-                .addClass("form-control")
-                .val(text);
-
-            $(this).html(textInput);
-            textInput.trigger("focus");
-        }
-    })
-
-    //save button
-    $(".saveBtn").click(function() {
-        replaceText($(this));
-    })
-
-    //update time on page
-    timeHour = 3600000 - today.milliseconds();
-    setTimeout(function() {
-        setInterval(checkTasks, 3600000)
-            
-        }, timeHour);
-    
-    getTasks();
+//save button
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    var blockId = parseInt(
+        $(this)
+            .closest(".task-info")
+            .attr("id")
+    );
+    var userInput = $.trim(
+        $(this)
+            .parent()
+            .find("textarea")
+            .val()
+    );
+    workTasks[blockId].task = userInput;
+    saveTasks();
+});
